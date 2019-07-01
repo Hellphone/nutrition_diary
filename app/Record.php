a901cdc30a3b6ca4f29116bb971a2a48bc0328f8
Nikita <inoy2005@inbox.ru>
2019-07-01 19:38:05 +0300

    Handling Kcal calculation exceptions

--- app/Record.php
+++ app/Record.php
@@ -12,18 +12,22 @@ class Record extends Model
         'date',
     ];
 
-    private $basicWeight = 100;
+    const BASIC_WEIGHT = 100;
 
-    private $proteinKcal = 4;
-    private $fatKcal = 9;
-    private $carbKcal = 4;
+    const PROTEIN_KCAL = 4;
+    const FAT_KCAL = 9;
+    const CARB_KCAL = 4;
 
     public function calculateKcal()
     {
-        $relation = $this->basicWeight / $this->weight;
-        $Kcal = ($this->product->proteins * $this->proteinKcal
-            + $this->product->fats * $this->fatKcal
-            + $this->product->carbs * $this->carbKcal) / $relation;
+        try {
+            $relation = self::BASIC_WEIGHT / $this->weight;
+            $Kcal = round(($this->product->proteins * self::PROTEIN_KCAL
+                    + $this->product->fats * self::FAT_KCAL
+                    + $this->product->carbs * self::CARB_KCAL) / $relation);
+        } catch (\Exception $e) {
+            return 0;
+        }
 
         return $Kcal;
     }
