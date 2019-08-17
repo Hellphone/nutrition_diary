08870a6fe0d8110f5af6a9475c97193bb8404deb
Nikita <inoy2005@inbox.ru>
2019-08-17 18:42:19 +0300

    Add Select2 to Product input

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -63,6 +63,8 @@
             margin-bottom: 30px;
         }
     </style>
+
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.8/css/select2.min.css" rel="stylesheet" />
 </head>
 <body>
 <div class="flex-center position-ref full-height">
@@ -85,19 +87,12 @@
             @yield('h1')
         </div>
 
-        {{--<div class="links">--}}
-            {{--<a href="https://laravel.com/docs">Docs</a>--}}
-            {{--<a href="https://laracasts.com">Laracasts</a>--}}
-            {{--<a href="https://laravel-news.com">News</a>--}}
-            {{--<a href="https://blog.laravel.com">Blog</a>--}}
-            {{--<a href="https://nova.laravel.com">Nova</a>--}}
-            {{--<a href="https://forge.laravel.com">Forge</a>--}}
-            {{--<a href="https://github.com/laravel/laravel">GitHub</a>--}}
-        {{--</div>--}}
-
         @yield('content')
 
     </div>
 </div>
+
+@yield('pagespecificscripts')
+
 </body>
 </html>
--- resources/views/records/create.blade.php
+++ resources/views/records/create.blade.php
@@ -8,6 +8,12 @@
     Create Record
 @endsection
 
+@section('pagespecificscripts')
+    <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.8/js/select2.min.js"></script>
+    <script src="{{ URL::asset('js/script.js') }}"></script>
+@endsection
+
 @section('content')
     <form method="POST" action="/records">
         @csrf
@@ -16,7 +22,7 @@
             <label for="product_id" class="label">Продукт</label>
 
             <div class="control">
-                <select name="product_id">
+                <select class="select2-basic-single" name="product_id">
                     @foreach ($products as $product)
                         <option value="{{ $product->id }}" @if ($product->id == old('product_id')) selected @endif >{{ $product->name }}</option>
                     @endforeach
