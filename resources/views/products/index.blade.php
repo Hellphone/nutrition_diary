efddc3e65919d6d774abb4378d971bb2f49cc484
Nikita <inoy2005@inbox.ru>
2020-07-21 18:28:40 +0300

    Add product editing on index page

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -20,6 +20,16 @@
         <ul class="table-like">
         @foreach($products as $product)
             <li>
+                <form class="form__update" method="POST" action="/products/{{ $product->id }}">
+                    @method('PATCH')
+                    @csrf
+                    <input type="text" class="input-editable" name="name" value="{{ $product->name }}">
+                    <input type="text" class="input-editable" name="proteins" value="{{ $product->proteins }}">
+                    <input type="text" class="input-editable" name="fats" value="{{ $product->fats }}">
+                    <input type="text" class="input-editable" name="carbs" value="{{ $product->carbs }}">
+                    <button type="submit">Update</button>
+                </form>
+                @include('errors')
                 <a href="/products/{{ $product->id }}">{{ $product->name }}</a>:
                 {{ $product->proteins }}|
                 {{ $product->fats }}|
